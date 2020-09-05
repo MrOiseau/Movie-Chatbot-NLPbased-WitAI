8f726426bd299aea63fcdb50c26af6dae52f9c8a
Nikola Bijanic <nikolabijanic93@gmail.com>
2020-09-05 20:17:40 +0200

    Undo MVC

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const homepageController = require('../controllers/homepageController');
-const chatBotController = require('../controllers/chatbotController');
+//const chatBotController = require('../controllers/chatBotController');
 
 let router = express.Router();
 
@@ -8,16 +8,13 @@ let initWebRoutes = (app)=> {
 
     router.get('/', homepageController.getHomepage);
                   /*(req, res) => {
-        return res.send('Zdraaavo svete! :)'); */
-
-    // Route handler for registering the webhook (app endpoint)	
-    router.get('/webhook', chatBotController.getWebhook);
-    // Receive all incoming messages from Fb
-    router.post('/webhook', chatBotController.postWebhookVerify);
-    router.post('/webhook', chatBotController.postWebhook);
-
+        return res.send('Zdraaavo svete! :)');
+        ////f.registerHook(req, res); });     */
     
-    return app.use('/', router);
+    ////router.get("/webhook", chatBotController.getWebhook);
+    ////router.post("/webhook", chatBotController.postWebhook);
+
+    return app.use("/", router);
 };
 
 module.exports = initWebRoutes;
